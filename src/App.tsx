ee989dc13f3e80f12e8e8c6a02d48ff3f4951dbf
Rupesh Ghosh <rupeshghosh10@gmail.com>
2025-01-03 18:07:56 +0530

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { BrowserRouter, Route, Routes } from 'react-router';
+import Home from './pages/home/home';
+
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
