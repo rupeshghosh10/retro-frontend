51675f06f465c1ab4c16c918299eb7c6892bfb77
Rupesh Ghosh <rupeshghosh10@gmail.com>
2025-01-05 15:44:41 +0530

    Make home page responsive

--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -5,18 +5,18 @@ import RoutesEnum from '@/routesEnum';
 const Home = () => {
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center">
-      <div className="prose">
+      <div className="prose text-center">
         <h1 className="text-5xl">Welcome to Retro App</h1>
-        <p className="text-center text-2xl">
+        <p className="text-lg sm:text-2xl">
           Create or join anonymous boards instantly. No login required, just pure collaboration.
         </p>
       </div>
-      <div className="mt-12 flex gap-5">
-        <Link to={RoutesEnum.CreateBoard} className="btn btn-primary btn-lg">
+      <div className="mt-12 flex flex-col gap-5 sm:flex-row">
+        <Link to={RoutesEnum.CreateBoard} className="btn btn-primary btn-md sm:btn-lg">
           <PlusIcon className="h-7 w-7" />
           <span>Create Board</span>
         </Link>
-        <button className="btn btn-primary btn-lg">
+        <button className="btn btn-primary btn-md sm:btn-lg">
           <UserGroupIcon className="h-7 w-7" />
           <span>Join a Board</span>
         </button>
